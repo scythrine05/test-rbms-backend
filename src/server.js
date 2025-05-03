eecc2eb0bf8a9aaa2a9a8633e8fe853a709e014a
logeshtv <logeshtv21@gmail.com>
2025-05-03 14:36:01 +0530

    changed the package json file

--- src/server.js
+++ src/server.js
@@ -1,29 +1,15 @@
 import app from "./app.js";
 import { Config } from "./config/index.js";
-import logger from "./config/logger.js";
 
 const startServer = async () => {
     const PORT = Config.PORT;
-    const NODE_ENV = Config.NODE_ENV;
-
-    if (NODE_ENV === "development") {
-        logger.info(`[✨] Environment: ${Config.NODE_ENV} 🛠️`);
-    } else if (NODE_ENV === "production") {
-        logger.info(`[✨] Environment: ${Config.NODE_ENV} 🌐`);
-    }
-
-    //Dispaly Timezone of the Server using Node
-    logger.info(`[✨] Timezone: ${Config.TZ} ⏰`);
-
-    //Display Offset of the Server Timezone
-    logger.info(`[✨] Timezone Offset: ${new Date().getTimezoneOffset()} minutes ⏰`);
 
     try {
         app.listen(PORT, () => {
-            logger.info(`[✨] Server Port: ${PORT} 🚀`);
+            console.log(`Server is running on port ${PORT}`);
         });
     } catch (err) {
-        logger.error(`[❌] Server : Failed >> Error: ${err.message}`);
+        console.error(`Server failed to start: ${err.message}`);
         process.exit(1);
     }
 };
