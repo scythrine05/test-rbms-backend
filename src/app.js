5352c5ae83875f95994512c7a2bf0203be99d587
Sreejith N Subramanian <nssreejith229@gmail.com>
2025-05-31 17:07:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import helmet from "helmet";
 //Config
 import { Config } from "./config/index.js";
 
-
 //Middlewares
 import api_compression from "./middlewares/api_compression.middleware.js";
 import routes from "./routes/index.js";
