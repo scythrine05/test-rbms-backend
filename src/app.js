9cb86c8a58f592ca3d5879a509c625c37bd6c4e4
vijayasvj <vj.as.entrepreneur@gmail.com>
2025-05-31 17:04:49 +0530

    Increase JSON and URL-encoded body size limits in app.js to 50mb

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ const app = express();
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(api_compression);
 
 //Root Route
