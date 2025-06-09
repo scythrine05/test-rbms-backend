0cfdcf8c71ca5b2ab2112f22516a712cd2189780
Rohan Murmu <scythrine05@gmail.com>
2025-06-09 17:59:25 +0530

    🔑 Add open apis with api key auth

---
+++ src/controllers/open.controller.js
@@ -0,0 +1,36 @@
+import {
+    fetchSanctionedRequests,
+    updateSanctionedRequestAvailed,
+} from "../services/open.service.js";
+import {
+    getSanctionedRequestsSchema,
+    patchSanctionedRequestSchema,
+} from "../validations/open.validation.js";
+import { handleError, successResponse } from "../utils/response.js";
+
+export const getSanctionedRequests = async (req, res) => {
+    try {
+        const { start_date, end_date } = getSanctionedRequestsSchema.parse(req.query);
+        const sanctionedRequests = await fetchSanctionedRequests(start_date, end_date);
+        return successResponse(
+            res,
+            200,
+            "Sanctioned requests fetched successfully",
+            sanctionedRequests,
+        );
+    } catch (error) {
+        handleError(error, res);
+    }
+};
+
+export const patchSanctionedRequest = async (req, res) => {
+    try {
+        const { id } = patchSanctionedRequestSchema.pick({ id: true }).parse(req.query);
+        const { availed } = patchSanctionedRequestSchema.pick({ availed: true }).parse(req.body);
+
+        const updatedRequest = await updateSanctionedRequestAvailed(id, availed);
+        return successResponse(res, 200, "Sanctioned request updated successfully", updatedRequest);
+    } catch (error) {
+        handleError(error, res);
+    }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import express from "express";
 import authRoute from "./auth.route.js";
 import userRequestRoute from "./user.request.route.js";
 import officerRoute from "./officer.route.js";
+import openRoutes from "./open.route.js";
+
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/user-request", userRequestRoute);
 router.use("/officer", officerRoute);
-// ghh
+router.use("/open", openRoutes);
 
 export default router;
