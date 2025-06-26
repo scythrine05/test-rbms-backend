df560516e6939554a50949a589cbeebbfdeff01c
Vijay Murugadoss <65366176+vijayasvj@users.noreply.github.com>
2025-06-26 09:32:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { Config } from "./config/index.js";
 import api_compression from "./middlewares/api_compression.middleware.js";
 import routes from "./routes/index.js";
 
+
 //express init
 const app = express();
 
