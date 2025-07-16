4e65c9b47db6731bf96de93e49bd6d566516c4f3
Rohan Murmu <scythrine05@gmail.com>
2025-07-16 13:16:56 +0530

    Add CUG no. filter in sanctioned get API

--- src/services/open.service.js
+++ src/services/open.service.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const trackMachinesPath = path.join(__dirname, "..", "data", "noOfTrackMachines.json");
 const trackMachinesData = JSON.parse(fs.readFileSync(trackMachinesPath, "utf8"));
 
-export const fetchSanctionedRequests = async (startDate, endDate) => {
+export const fetchSanctionedRequests = async (startDate, endDate, CUG) => {
     const where = {
         isSanctioned: true,
     };
@@ -23,6 +23,12 @@ export const fetchSanctionedRequests = async (startDate, endDate) => {
         where.sanctionedTimeTo = Object.assign(where.sanctionedTimeTo || {}, {
             lte: new Date(endDate),
         });
+    }
+
+    if (CUG) {
+        where.user = {
+            phone: CUG,
+        };
     }
 
     const requests = await prisma.request.findMany({
